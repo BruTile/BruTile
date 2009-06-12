ab83cb426e2409f8e6674f43600cc7fb24fc862f
pauldendulk <pauldendulk@localhost>
2009-06-12 11:40:51 +0000

    Added Compact Framework project file.

--- BruTile/Properties/AssemblyInfo.cs
+++ BruTile/Properties/AssemblyInfo.cs
@@ -33,4 +33,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.2.*")]
+#if (!PocketPC)
 [assembly: AssemblyFileVersion("0.2.0.0")]
+#endif
