1ca7b2fb5bf6621ea351f7747aee4349696dcb6d
pauldendulk <pauldendulk@localhost>
2009-06-12 06:48:06 +0000

    added ITileProvider to prepare for local tile rendering

---
+++ BruTile/WebTileProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tiling
+{
+  public class WebTileProvider : ITileProvider
+  {
+    IRequestBuilder requestBuilder;
+    
+    public WebTileProvider(IRequestBuilder requestBuilder)
+    {
+      this.requestBuilder = requestBuilder;
+    }
+
+    #region TileProvider Members
+
+    public byte[] GetTile(TileInfo tileInfo)
+    {
+      Uri requestUrl = requestBuilder.GetUrl(tileInfo);
+      return ImageRequest.GetImageFromServer(requestUrl);
+    }
+
+    #endregion
+  }
+}
