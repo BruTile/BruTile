029340ba213fb21652558903a4755eecd58008a2
Paul den Dulk <pauldendulk@users.noreply.github.com>
2009-06-17 11:25:47 +0000

    Adding RequestBasic

---
+++ BruTile/RequestBasic.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Tiling
+{
+    /// <summary>
+    /// A flexible request builder that can be used for a number of simple cases.
+    /// </summary>
+    public class RequestBasic : IRequestBuilder
+    {
+        string urlFormatter;
+
+        public RequestBasic(string urlFormatter)
+        {
+            this.urlFormatter = urlFormatter;
+        }
+
+        public Uri GetUrl(TileInfo tile)
+        {
+            string result = String.Format(
+              CultureInfo.InvariantCulture, urlFormatter, 
+              tile.Key.Level, tile.Key.Col, tile.Key.Row);
+
+            return new Uri(result);
+        }
+
+    }
+}
