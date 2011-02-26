9b28f284da21faf920f30a77e385cc85bb037e95
Paul den Dulk <pauldendulk@users.noreply.github.com>
2011-02-26 07:45:19 +0000

    Minor change to OsmTileSource

---
+++ BruTile/Cache/CachingTileProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BruTile.Cache
+{
+    class CachingTileProvider : ITileProvider
+    {
+        private ITileProvider provider;
+        private ITileCache<byte[]> cache;
+
+        public CachingTileProvider(ITileProvider provider, ITileCache<byte[]> cache)
+        {
+            this.provider = provider;
+        }
+
+        public byte[] GetTile(TileInfo tileInfo)
+        {
+            byte[] bytes = cache.Find(tileInfo.Index);
+
+            if (bytes == null)
+            {
+                bytes = provider.GetTile(tileInfo);
+                if (bytes != null) cache.Add(tileInfo.Index, bytes);
+            }
+            return bytes;
+        }
+    }
+}
