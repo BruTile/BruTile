71d8fa0423e385e5523c63cb5c4ffe230b9b2f97
Paul den Dulk <pauldendulk@users.noreply.github.com>
2012-07-08 23:47:36 +0200

    fixed unit test WmsCapabilities_WhenParsed_ShouldSetCorrectGetMapUrl

--- BruTile/Web/Wms/Request.cs
+++ BruTile/Web/Wms/Request.cs
@@ -35,7 +35,7 @@ namespace BruTile.Web.Wms
                         GetFeatureInfo = new OperationType(element, @namespace);
                         break;
                     default:
-                        ExtendedOperation.Add(node.Name, new OperationType(element, @namespace));
+                        ExtendedOperation.Add(element.Name.LocalName, new OperationType(element, @namespace));
                         break;
                 }
             }
