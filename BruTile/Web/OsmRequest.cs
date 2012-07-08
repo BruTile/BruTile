e3f581f04069fdd45d7f04b198fd2983eae16e55
pauldendulk <pauldendulk@localhost>
2012-07-08 14:04:00 +0200

    some more comment updates

--- BruTile/Web/OsmRequest.cs
+++ BruTile/Web/OsmRequest.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) BruTile developers team. All rights reserved. See License.txt in the project root for license information.
 
-// This file was created by Felix Obermaier, 2011, Ingenieurgruppe IVV GmbH & Co. KG, www.ivv-aachen.de;
+// This file was created by Felix Obermaier (www.ivv-aachen.de) 2011.
 
 using System;
 
