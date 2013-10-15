85626c80b93ee7a741ed4f750cf339f9989e4956
Felix Obermaier <FObermaier@users.noreply.github.com>
2013-10-15 15:45:16 +0200

    WmsCapabilities, this one, too

--- BruTile.Tests/Web/TileSourceWmsCTest.cs
+++ BruTile.Tests/Web/TileSourceWmsCTest.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Xml.Linq;
@@ -31,6 +33,28 @@ namespace BruTile.Tests.Web
                     Assert.NotNull(tileSource.Schema.Extent);
                     Assert.NotNull(tileSource.Schema.Srs);
                 }
+            }
+        }
+
+        [Test]
+        public void TestUrls()
+        {
+            TestUrl("http://demo.opengeo.org/geoserver/gwc/service/wms?request=getcapabilities&tiled=true");
+            //TestUrl("http://www.osmgb.org.uk/ogc/wmsc?Version=1.1.1&Service=WMS&Request=GetCapabilities");
+        }
+
+        private static void TestUrl(string url)
+        {
+            IEnumerable<ITileSource> sources = null;
+            Assert.DoesNotThrow(() => sources = WmscTileSource.CreateFromWmscCapabilties(new Uri(url)), string.Format("Failed for '{0}'", url));
+            foreach (var tileSource in sources)
+            {
+                Assert.NotNull(tileSource.Provider);
+                Assert.NotNull(tileSource.Schema);
+                Assert.NotNull(tileSource.Schema.Resolutions);
+                Assert.NotNull(tileSource.Schema.Axis);
+                Assert.NotNull(tileSource.Schema.Extent);
+                Assert.NotNull(tileSource.Schema.Srs);
             }
         }
     }
