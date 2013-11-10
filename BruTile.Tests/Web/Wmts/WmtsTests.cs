047497a16727600f21c525b66ca96caa0c45d0c0
pauldendulk <pauldendulk@localhost>
2013-11-10 12:33:26 +0100

    added generated classes for wmts getcapabilities request.

---
+++ BruTile.Tests/Web/Wmts/WmtsTests.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using BruTile.Web;
+using NUnit.Framework;
+
+namespace BruTile.Tests.Web.Wmts
+{
+    [TestFixture]
+    public class WmtsTests
+    {
+        [Test]
+        public void TestParsingWmtsCapabilities()
+        {
+            // arrange
+            using (var stream = File.OpenRead(Path.Combine("Resources", "Wmts", "wmts-capabilties-copied-from-openlayers-sample.xml")))
+            {
+                // act
+                var ser = new XmlSerializer(typeof(Capabilities));
+
+                Capabilities capabilties;
+
+                using (var reader = new StreamReader(stream))
+                {
+                     capabilties = (Capabilities)ser.Deserialize(reader);
+                }
+                // assert
+                Assert.NotNull(capabilties);
+            }
+        }
+    }
+}
