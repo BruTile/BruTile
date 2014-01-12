7a11a94adf81cdd5508129c2b4ec00987fa3008a
pauldendulk <pauldendulk@localhost>
2014-01-12 23:41:51 +0100

    moving build.proj to scripts

--- Scripts/buildpack.bat
+++ Scripts/buildpack.bat
@@ -3,11 +3,10 @@ SETLOCAL
 SET VERSION=%1
 SET NUGET=.\..\.nuget\nuget.exe
 
-msbuild /t:BuildRelease .\..\build.proj /p:AsmVersion=%VERSION%
+msbuild /t:BuildRelease .\build.proj /p:AsmVersion=%VERSION%
 %NUGET% pack brutile.nuspec -Version %VERSION% -outputdirectory .\..\Release
 %NUGET% pack brutile.desktop.nuspec -Version %VERSION% -outputdirectory .\..\Release
-%NUGET% push .\..\Release\brutile.%VERSION%.nupkg 
-%NUGET% push .\..\Release\brutile.desktop.%VERSION%.nupkg 
+
 
 
 
