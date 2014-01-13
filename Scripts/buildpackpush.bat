3227e89d7f03f8233c993a23a143bff8818d613c
pauldendulk <pauldendulk@localhost>
2014-01-13 16:26:31 +0100

    Adding CALL in buildpackpush.bat

--- Scripts/buildpackpush.bat
+++ Scripts/buildpackpush.bat
@@ -3,7 +3,7 @@ SETLOCAL
 SET VERSION=%1
 SET NUGET=.\..\.nuget\nuget.exe
 
-buildpack %VERSION%
+CALL buildpack %VERSION%
 %NUGET% push .\..\Release\brutile.%VERSION%.nupkg 
 %NUGET% push .\..\Release\brutile.desktop.%VERSION%.nupkg 
 
