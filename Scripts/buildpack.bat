a9be3560718c9a0108b5c99d3b01ef945ade409d
Paul den Dulk <pauldendulk@users.noreply.github.com>
2014-02-06 23:51:46 +0100

    update build script

--- Scripts/buildpack.bat
+++ Scripts/buildpack.bat
@@ -3,7 +3,8 @@ SETLOCAL
 SET VERSION=%1
 SET NUGET=.\..\.nuget\nuget.exe
 
-msbuild /t:BuildRelease .\build.proj /p:AsmVersion=%VERSION%
+msbuild updateversionnumber.proj /p:AsmVersion=%VERSION%
+msbuild build.proj 
 %NUGET% pack brutile.nuspec -Version %VERSION% -outputdirectory .\..\Release
 %NUGET% pack brutile.desktop.nuspec -Version %VERSION% -outputdirectory .\..\Release
 
