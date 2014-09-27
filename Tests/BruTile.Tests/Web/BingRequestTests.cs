cad7a6a80c5ac5682e9511f9b7d80b68524049bc
pauldendulk <pauldendulk@localhost>
2014-09-27 19:36:10 +0200

    added failing unit test on multithreaded bing requests

--- Tests/BruTile.Tests/Web/BingRequestTests.cs
+++ Tests/BruTile.Tests/Web/BingRequestTests.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
+using System.Threading.Tasks;
 using BruTile.Web;
 using NUnit.Framework;
 
@@ -23,6 +26,24 @@ namespace BruTile.Tests.Web
 
             // assert
             Assert.True(url2.ToString() == "http://t111.tiles.virtualearth.net/tiles/r00211.jpeg?g=555&token=pindakaas");
+        }
+
+        [Test]
+        public void GetUriMultiThreadedTest()
+        {
+            // arrange
+            var request = new BingRequest(
+                "http://t{s}.tiles.virtualearth.net/tiles/r{quadkey}.jpeg?g={apiversion}&token={userkey}",
+                "pindakaas", "555", new[] { "000", "111" });
+            var tileInfo = new TileInfo { Index = new TileIndex(3, 4, "5") };
+
+            // act
+            var requests = new List<Func<Uri>>();
+            for (var i = 0 ; i < 100; i++) requests.Add(() => request.GetUri(tileInfo));
+            Parallel.ForEach(requests, r =>  r());
+
+            // assert
+            //Assert.True(url2.ToString() == "http://t111.tiles.virtualearth.net/tiles/r00211.jpeg?g=555&token=pindakaas");
         }
     }
 }
