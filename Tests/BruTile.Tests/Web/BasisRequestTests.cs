858052ea45ae33d7288eb0c5a91d6f6c9bea6fe3
Paul den Dulk <pauldendulk@users.noreply.github.com>
2014-10-15 21:50:34 +0200

    fixed BasicRequest's issue with the _nodeCounter when multithreaded

--- Tests/BruTile.Tests/Web/BasisRequestTests.cs
+++ Tests/BruTile.Tests/Web/BasisRequestTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using BruTile.Web;
 using NUnit.Framework;
 
@@ -21,5 +23,25 @@ namespace BruTile.Tests.Web
             // assert
             Assert.True(url.ToString() == "http://a.tile.openstreetmap.org/5/3/4.png");
         }
+
+        [Test]
+        public void GetUriInParallelTest()
+        {
+            // arrange
+            var request = new BasicRequest("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", new[] {"a", "b", "c"});
+            var tileInfo = new TileInfo {Index = new TileIndex(3, 4, "5")};
+            var urls = new List<Uri>();
+
+            // act
+            var requests = new List<Func<Uri>>();
+            for (var i = 0; i < 100; i++) requests.Add(() => request.GetUri(tileInfo));
+            Parallel.ForEach(requests, r => urls.Add(r()));
+
+            // assert
+            Assert.True(urls.FirstOrDefault(u => u.ToString() == "http://b.tile.openstreetmap.org/5/3/4.png") != null);
+        }
     }
 }
+
+
+
