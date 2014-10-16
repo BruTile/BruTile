0479c498ad72f23336d5b4e88e8fabf3fe0831fb
Paul den Dulk <pauldendulk@users.noreply.github.com>
2014-10-16 17:06:09 +0200

    Creating new project BruTile.Samples.MbTiles. Moving files from WinFormsSample.

--- Samples/BruTile.Samples.MbTiles/Program.cs
+++ Samples/BruTile.Samples.MbTiles/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Windows.Forms;
+using WinFormsSample;
 
 namespace BruTile.Samples.MbTiles
 {
@@ -15,7 +14,7 @@ namespace BruTile.Samples.MbTiles
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-           // Application.Run(new Form1());
+            Application.Run(new MbTilesForm());
         }
     }
 }
