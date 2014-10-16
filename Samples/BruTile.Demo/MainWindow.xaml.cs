df81ed18338250e7fce6085676cc1425835b89f6
Paul den Dulk <pauldendulk@users.noreply.github.com>
2014-10-16 12:22:21 +0200

    Not using cloudmade in samples

--- Samples/BruTile.Demo/MainWindow.xaml.cs
+++ Samples/BruTile.Demo/MainWindow.xaml.cs
@@ -1,12 +1,11 @@
-﻿using System;
+﻿using BruTile.Web;
+using System;
 using System.Linq;
-using System.Windows;
 using System.Windows.Controls;
-using BruTile.Web;
 
 namespace BruTile.Demo
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
@@ -18,8 +17,8 @@ namespace BruTile.Demo
                 radioButton.Click += (sender, args) =>
                     {
                         var knownTileServer = (KnownTileServers)((RadioButton)sender).Tag;
-                        var apiKey = (knownTileServer.ToString().ToLower().Contains("cloudmade")) ? "481143122a9445f4a94aee9c67a5151d" : null;
-                        MapControl.SetTileSource(TileSource.Create(knownTileServer, apiKey));
+                        if (!knownTileServer.ToString().ToLower().Contains("cloudmade")) // Exclude CloudMade
+                            MapControl.SetTileSource(TileSource.Create(knownTileServer));
                     };
                 Layers.Children.Add(radioButton);
             }
