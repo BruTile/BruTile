ee648632da80a8f7524420bcf39ab06f44920636
Paul den Dulk <pauldendulk@users.noreply.github.com>
2014-10-16 17:32:52 +0200

    Adding google to BruTile.Demo

--- Samples/BruTile.Demo/MainWindow.xaml.cs
+++ Samples/BruTile.Demo/MainWindow.xaml.cs
@@ -11,18 +11,31 @@ namespace BruTile.Demo
         {
             InitializeComponent();
 
-            foreach (var layer in  Enum.GetValues(typeof(KnownTileServers)).Cast<KnownTileServers>())
+            foreach (var knownTileServer in  Enum.GetValues(typeof(KnownTileServers)).Cast<KnownTileServers>())
             {
-                if (layer.ToString().ToLower().Contains("cloudmade")) continue; // Exclude CloudMade
-                
-                var radioButton = new RadioButton {Content = layer.ToString(), Tag = layer};
-                radioButton.Click += (sender, args) =>
-                    {
-                        var knownTileServer = (KnownTileServers)((RadioButton)sender).Tag;
-                        MapControl.SetTileSource(TileSource.Create(knownTileServer));
-                    };
+                if (knownTileServer.ToString().ToLower().Contains("cloudmade")) continue; // Exclude CloudMade
+
+                KnownTileServers server = knownTileServer;
+                var radioButton = ToRadioButton(knownTileServer.ToString(), () => TileSource.Create(server));
                 Layers.Children.Add(radioButton);
             }
+
+            Layers.Children.Add(ToRadioButton("Google Map", () => new GoogleTileSource(GoogleMapType.GoogleMap)));
+            Layers.Children.Add(ToRadioButton("Google Satellite", () => new GoogleTileSource(GoogleMapType.GoogleSatellite)));
+            Layers.Children.Add(ToRadioButton("Google Labels", () => new GoogleTileSource(GoogleMapType.GoogleLabels)));
+            Layers.Children.Add(ToRadioButton("Google Terrain", () => new GoogleTileSource(GoogleMapType.GoogleTerrain)));
+        }
+
+        private RadioButton ToRadioButton(string name, Func<ITileSource> func)
+        {
+            var radioButton = new RadioButton
+            {
+                Content = name,
+                Tag = new Func<ITileSource>(func)
+            };
+            radioButton.Click += (sender, args) => MapControl.SetTileSource(((Func<ITileSource>)((RadioButton)sender).Tag)());
+
+            return radioButton;
         }
     }
 }
