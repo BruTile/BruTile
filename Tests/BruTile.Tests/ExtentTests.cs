38c9a45145cff2b48cf6ef9abb6c9fe447bdeed3
pauldendulk <pauldendulk@gmail.com>
2014-11-12 13:24:16 +0100

    Adding ExtentTests.cs

---
+++ Tests/BruTile.Tests/ExtentTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace BruTile.Tests
+{
+    [TestFixture]
+    public class ExtentTests
+    {
+        [Test]
+        public void TestEquals()
+        {
+            // arrange
+            var extent = new Extent(-180, -90, 180, 90);
+            var extentSame = new Extent(-180, -90, 180, 90);
+            var extentDiffMinX = new Extent(-181, -90, 180, 90);
+            var extentDiffMinY = new Extent(-180, -91, 180, 90);
+            var extentDiffMaxX = new Extent(-180, -90, 181, 90);
+            var extentDiffMaxy = new Extent(-180, -90, 180, 91);
+            
+            // act + assert
+            Assert.True(extent == extentSame);
+            Assert.False(extent != extentSame);
+            Assert.False(extent == extentDiffMinX);
+            Assert.False(extent == extentDiffMinY);
+            Assert.False(extent == extentDiffMaxX);
+            Assert.False(extent == extentDiffMaxy);
+        }
+
+        [Test]
+        public void TestMinSmallerThanMax()
+        {
+            // arrange + act + assert
+            Assert.Throws<ArgumentException> (() => new Extent(-180, 90, 180, -90));
+        }
+    }
+}
