13c5526b7a30baa542ac2998160f212c8100661d
Josh Cooley <josh.cooley@intergraph.com>
2014-12-21 16:46:54 -0600

    Initial Performance Tests
    Create a Performance folder for unit tests focused on measuring
    performance numbers.  Initial test is measuring FileCache.Add and
    FileCache.Find.

---
+++ Performance/BruTile.Desktop.Tests/Cache/FileCacheTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using BruTile.Cache;
+using NUnit.Framework;
+
+namespace BruTile.PerformanceTests.Cache
+{
+    [TestFixture, Category("CacheTest")]
+    class FileCacheTests
+    {
+        private FileCache _cache;
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            _cache = ClearedFileCacheTest();
+        }
+
+        [Test]
+        public void LoadTilesAndFindTiles()
+        {
+            var threadCount = 500;
+            var loadWork = new WorkTimer(threadCount);
+            var findWork = new WorkTimer(threadCount);
+            loadWork.TimeWork(
+                i => new
+                {
+                    TileIndex = new TileIndex(i % 10, i / 10, "1"),
+                    Image = new byte[] { (byte)i }
+                },
+                args => _cache.Add(args.TileIndex, args.Image));
+            loadWork.WaitForTestsToComplete();
+            findWork.TimeWork(i => new TileIndex(i % 10, i / 10, "1"), index => _cache.Find(index));
+            findWork.WaitForTestsToComplete();
+            Console.WriteLine("Total FileCache.Add time is {0}ms", loadWork.TotalTime);
+            Console.WriteLine("Average FileCache.Add time is {0}ms", loadWork.TotalTime / threadCount);
+            Console.WriteLine("Max FileCache.Add time is {0}ms", loadWork.MaxTime);
+            Console.WriteLine("Min FileCache.Add time is {0}ms", loadWork.MinTime);
+            Console.WriteLine("Total FileCache.Find time is {0}ms", findWork.TotalTime);
+            Console.WriteLine("Average FileCache.Find time is {0}ms", findWork.TotalTime / threadCount);
+            Console.WriteLine("Max FileCache.Find time is {0}ms", findWork.MaxTime);
+            Console.WriteLine("Min FileCache.Find time is {0}ms", findWork.MinTime);
+        }
+
+        private static FileCache ClearedFileCacheTest()
+        {
+            if (Directory.Exists("FileCacheTest"))
+                Directory.Delete("FileCacheTest", true);
+            return new FileCache("FileCacheTest", "buf");
+        }
+    }
+}
---
+++ Performance/BruTile.Desktop.Tests/WorkTimer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BruTile.PerformanceTests
+{
+
+    class WorkTimer
+    {
+        private long _maxTime = long.MinValue;
+        private long _minTime = long.MaxValue;
+        private long _totalTime;
+        private int _threadCompleteCount;
+        private int _testCount;
+        private object _syncRoot = new object();
+
+        public long MaxTime { get { return _maxTime; } }
+        public long MinTime { get { return _minTime; } }
+        public long TotalTime { get { return _totalTime; } }
+
+        public WorkTimer(int testCount)
+        {
+            _testCount = testCount;
+        }
+
+        public void TimeWork(Action work)
+        {
+            for (int i = 0; i < _testCount; i++)
+            {
+                Task.Run(() => TimeSingleWork(work));
+            }
+        }
+
+        public void TimeWork<T>(Func<int, T> argFactory, Action<T> workT)
+        {
+            for (int i = 0; i < _testCount; i++)
+            {
+                var arg = argFactory(i);
+                Action work = () => workT(arg);
+                Task.Run(() => TimeSingleWork(work));
+            }
+        }
+
+        public void WaitForTestsToComplete()
+        {
+            while (Interlocked.CompareExchange(ref _threadCompleteCount, 0, _testCount) != _testCount)
+            {
+                Thread.Sleep(50);
+            }
+        }
+
+        private void TimeSingleWork(Action action)
+        {
+            var stopwatch = Stopwatch.StartNew();
+            try
+            {
+                action();
+            }
+            finally
+            {
+                stopwatch.Stop();
+                lock (_syncRoot)
+                {
+                    _totalTime += stopwatch.ElapsedMilliseconds;
+                    _minTime = Math.Min(_minTime, stopwatch.ElapsedMilliseconds);
+                    _maxTime = Math.Max(_maxTime, stopwatch.ElapsedMilliseconds);
+                }
+                Interlocked.Increment(ref _threadCompleteCount);
+            }
+        }
+    }
+}
