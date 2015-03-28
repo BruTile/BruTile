db37216946236f13f38e8fc265f470f23e1adb78
pauldendulk <pauldendulk@gmail.com>
2015-03-28 20:30:21 +0100

    Adding test method to demonstrate .NET 4.5 extension method on HttpTileSource

---
+++ Tests/BruTile.Tests/Web/HttpTileSourceExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using BruTile.Web;
+
+namespace BruTile.Samples.AsyncRequest
+{
+    public static class HttpTileSourceExtensions
+    {
+        public static async Task<byte[][]> GetTilesAsync(this HttpTileSource tileSource, HttpClient httpClient, IEnumerable<TileInfo> tileInfos)
+        {
+            var tasks = tileInfos.Select(t => tileSource.GetTileAsync(httpClient, t)).ToArray();
+            var result = Task.WhenAll(tasks);
+            return await result;
+        }
+
+        public static async Task<byte[]> GetTileAsync(this HttpTileSource tileSource, HttpClient httpClient, TileInfo tileInfo)
+        {
+            var bytes = tileSource.PersistentCache.Find(tileInfo.Index);
+            if (bytes != null) return bytes;
+            bytes = await httpClient.GetByteArrayAsync(tileSource.GetUri(tileInfo));
+            if (bytes != null) tileSource.PersistentCache.Add(tileInfo.Index, bytes);
+            return bytes;
+        }
+    }
+}
---
+++ Tests/BruTile.Tests/Web/HttpTileSourceTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime;
+using BruTile.Predefined;
+using BruTile.Samples.AsyncRequest;
+using NUnit.Framework;
+using RichardSzalay.MockHttp;
+
+namespace BruTile.Tests.Web
+{
+    [TestFixture]
+    public class HttpTileSourceTests
+    {
+        [Test]
+        public static async void TestAsyncTileFetcher()
+        {
+            // arrange
+            var tileSource = KnownTileSources.Create();
+            var mockHttp = new MockHttpMessageHandler();
+            mockHttp.When("http://*").Respond("image/png", new MemoryStream());
+            var httpClient = new HttpClient(mockHttp);
+            var range = tileSource.Schema.GetTileInfos(tileSource.Schema.Extent, "3");
+            var timeStart = DateTime.Now;
+
+            // act
+            var tiles = await tileSource.GetTilesAsync(httpClient, range);
+
+            // assert
+            Console.WriteLine("Durations: {0:0} milliseconds", DateTime.Now.Subtract(timeStart).TotalMilliseconds);
+            Assert.AreEqual(tiles.Count(), 64);
+        }
+    }
+}
