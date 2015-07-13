47641c95620c6101a649c6d672f70a560994e253
pauldendulk <pauldendulk@gmail.com>
2015-07-13 15:28:33 +0200

    Removing check on 'image' response to allow vector tiles

--- BruTile/Web/RequestHelper.cs
+++ BruTile/Web/RequestHelper.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) BruTile developers team. All rights reserved. See License.txt in the project root for license information.
 
 using System;
-using System.Globalization;
 using System.IO;
 using System.Net;
 using BruTile.Extensions;
@@ -28,16 +27,10 @@ namespace BruTile.Web
                     throw (new WebException("An error occurred while fetching tile", null));
                 }
 
-                if (webResponse.ContentType.StartsWith("image", StringComparison.OrdinalIgnoreCase))
+                using (Stream responseStream = webResponse.GetResponseStream())
                 {
-                    using (Stream responseStream = webResponse.GetResponseStream())
-                    {
-                        return Utilities.ReadFully(responseStream);
-                    }
+                    return Utilities.ReadFully(responseStream);
                 }
-
-                var message = ComposeErrorMessage(webResponse, webRequest.RequestUri.AbsoluteUri);
-                throw (new WebResponseFormatException(message, null));
             }
         }
 
@@ -54,38 +47,6 @@ namespace BruTile.Web
             }
 
             return FetchImage(webRequest);
-        }
-
-        private static string ComposeErrorMessage(WebResponse webResponse, string uri)
-        {
-            string message = String.Format(
-                CultureInfo.InvariantCulture,
-                "Failed to retrieve tile from this uri:\n{0}\n.An image was expected but the received type was '{1}'.",
-                uri,
-                webResponse.ContentType
-            );
-
-            if (webResponse.ContentType.StartsWith("text", StringComparison.OrdinalIgnoreCase))
-            {
-                using (Stream stream = webResponse.GetResponseStream())
-                {
-                    message += String.Format(CultureInfo.InvariantCulture,
-                      "\nThis was returned:\n{0}", ReadAllText(stream));
-                }
-            }
-            return message;
-        }
-
-        private static string ReadAllText(Stream responseStream)
-        {
-            using (var streamReader = new StreamReader(responseStream, true))
-            {
-                using (var stringWriter = new StringWriter(CultureInfo.InvariantCulture))
-                {
-                    stringWriter.Write(streamReader.ReadToEnd());
-                    return stringWriter.ToString();
-                }
-            }
         }
     }
 }
