9b4189ea42f9c65b4d5414a782914716b3d6bc3d
pauldendulk <pauldendulk@gmail.com>
2015-07-30 13:59:19 +0200

    Starting on vector tile sample

---
+++ Samples/BruTile.Samples.VectorTileToBitmap/SphericalMercator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Drawing;
+
+namespace BruTile.Samples.VectorTileToBitmap
+{
+    public class SphericalMercator
+    {
+        private const double Radius = 6378137;
+        private const double D2R = Math.PI / 180;
+        private const double HalfPi = Math.PI / 2;
+
+        public static PointF FromLonLat(double lon, double lat)
+        {
+            var lonRadians = (D2R * lon);
+            var latRadians = (D2R * lat);
+
+            var x = Radius * lonRadians;
+            var y = Radius * Math.Log(Math.Tan(Math.PI * 0.25 + latRadians * 0.5));
+
+            return new PointF((float)x, (float)y);
+        }
+
+        public static PointF ToLonLat(double x, double y)
+        {
+            var ts = Math.Exp(-y / (Radius));
+            var latRadians = HalfPi - 2 * Math.Atan(ts);
+
+            var lonRadians = x / (Radius);
+
+            var lon = (lonRadians / D2R);
+            var lat = (latRadians / D2R);
+
+            return new PointF((float)lon, (float)lat);
+        }
+    }
+}
---
+++ Samples/BruTile.Samples.VectorTileToBitmap/VectorTileConverter.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using GeoJSON.Net;
+using GeoJSON.Net.Geometry;
+using mapbox.vector.tile;
+
+namespace BruTile.Samples.VectorTileToBitmap
+{
+    public class VectorTileConverter
+    {
+        public byte[] ToBitmap(LayerInfo layerInfo)
+        {
+            using (var bitmap = new Bitmap(256, 256))
+            using (var canvas = Graphics.FromImage(bitmap))
+            using (var pen = new Pen(Color.Red, 2))
+            {
+                foreach (var feature in layerInfo.FeatureCollection.Features)
+                {
+                    if (feature.Geometry.Type == GeoJSONObjectType.Polygon)
+                    {
+                        var polygon = (Polygon)feature.Geometry;
+                        
+                        foreach (var lineString in polygon.Coordinates)
+                        {
+                            canvas.Transform = new Matrix(); // todo set te matrix transform to match the tile extent.
+                            canvas.DrawPolygon(pen, ToGdi(lineString));
+                        }
+                    }
+                }
+
+                return ToBytes(bitmap);
+            }
+        }
+
+        public PointF[] ToGdi(LineString lineString)
+        {
+            var result = new List<PointF>();
+
+            foreach (var coordinate in lineString.Coordinates)
+            {
+                var position = (GeographicPosition) coordinate;
+                result.Add(SphericalMercator.FromLonLat(position.Longitude, position.Latitude));
+
+            }
+
+            return result.ToArray();
+        }
+
+        public static byte[] ToBytes(Image img)
+        {
+            using (var stream = new MemoryStream())
+            {
+                img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                return stream.ToArray();
+            }
+        }
+    }
+}
