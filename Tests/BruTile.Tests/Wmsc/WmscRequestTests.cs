68151a2ba944b6fbe44ed036cf1c29907982921c
pauldendulk <pauldendulk@gmail.com>
2016-03-08 22:53:51 +0100

    Minor simplification unit tests WmscRequest

--- Tests/BruTile.Tests/Wmsc/WmscRequestTests.cs
+++ Tests/BruTile.Tests/Wmsc/WmscRequestTests.cs
@@ -12,7 +12,7 @@ namespace BruTile.Tests.Wmsc
         [Test]
         public void WmscRequest_NoVersion()
         {
-            var request = new WmscRequest(new Uri("http://testserver.com"), new GlobalSphericalMercator(YAxis.TMS), new List<string>(new [] { "Layer One" }), null);
+            var request = new WmscRequest(new Uri("http://testserver.com"), new GlobalSphericalMercator(YAxis.TMS), new [] { "Layer One" });
             var ti = new TileInfo {Index = new TileIndex(0, 0, "0")};
             var uri = request.GetUri(ti);
             StringAssert.DoesNotContain("VERSION=", uri.ToString());
@@ -22,7 +22,7 @@ namespace BruTile.Tests.Wmsc
         [Test]
         public void WmscRequest_Version111()
         {
-            var request = new WmscRequest(new Uri("http://testserver.com"), new GlobalSphericalMercator(YAxis.TMS), new List<string>(new[] { "Layer One" }), null, null, "1.1.1");
+            var request = new WmscRequest(new Uri("http://testserver.com"), new GlobalSphericalMercator(YAxis.TMS), new[] { "Layer One" }, null, null, "1.1.1");
             var ti = new TileInfo { Index = new TileIndex(0, 0, "0") };
             var uri = request.GetUri(ti);
             StringAssert.Contains("VERSION=1.1.1", uri.ToString());
@@ -32,7 +32,7 @@ namespace BruTile.Tests.Wmsc
         [Test]
         public void WmscRequest_Version130()
         {
-            var request = new WmscRequest(new Uri("http://testserver.com"), new GlobalSphericalMercator(YAxis.TMS), new List<string>(new[] { "Layer One" }), null, null, "1.3.0");
+            var request = new WmscRequest(new Uri("http://testserver.com"), new GlobalSphericalMercator(YAxis.TMS), new[] { "Layer One" }, null, null, "1.3.0");
             var ti = new TileInfo { Index = new TileIndex(0, 0, "0") };
             var uri = request.GetUri(ti);
             StringAssert.Contains("VERSION=1.3.0", uri.ToString());
