3e09c5c5aee218a821030c2e4f4f659c18efd539
pauldendulk <pauldendulk@gmail.com>
2016-03-12 17:41:23 +0100

    Reverting unwanted commit

--- Scripts/buildpackpush.bat
+++ Scripts/buildpackpush.bat
@@ -4,9 +4,9 @@ SET VERSION=%1
 SET NUGET=.\..\tools\nuget\nuget.exe
 
 CALL buildpack %VERSION%
-REM %NUGET% push .\..\Release\brutile.%VERSION%.nupkg 
-REM %NUGET% push .\..\Release\brutile.desktop.%VERSION%.nupkg 
-git commit -m ''%VERSION%''
-REM git tag %VERSION%
+%NUGET% push .\..\Release\brutile.%VERSION%.nupkg 
+%NUGET% push .\..\Release\brutile.desktop.%VERSION%.nupkg 
+git commit -m %VERSION%
+git tag %VERSION%
 git push
 
