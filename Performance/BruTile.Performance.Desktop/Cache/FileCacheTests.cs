d2c642009b59e007f1899baa51e8522417c0911a
pauldendulk <pauldendulk@gmail.com>
2017-10-15 10:07:01 +0200

    Fixing delete directory problem in unit test

--- Performance/BruTile.Performance.Desktop/Cache/FileCacheTests.cs
+++ Performance/BruTile.Performance.Desktop/Cache/FileCacheTests.cs
@@ -26,7 +26,7 @@ namespace BruTile.PerformanceTests.Cache
                 i => new
                 {
                     TileIndex = new TileIndex(i % 10, i / 10, "1"),
-                    Image = new byte[] { (byte)i }
+                    Image = new [] { (byte)i }
                 },
                 args => _cache.Add(args.TileIndex, args.Image));
             loadWork.WaitForTestsToComplete();
@@ -45,8 +45,33 @@ namespace BruTile.PerformanceTests.Cache
         private static FileCache ClearedFileCacheTest()
         {
             if (Directory.Exists("FileCacheTest"))
-                Directory.Delete("FileCacheTest", true);
+            {
+                DeleteDirectory("FileCacheTest");
+            }
             return new FileCache("FileCacheTest", "buf");
+        }
+
+        /// <summary>
+        /// From stackoverflow: https://stackoverflow.com/a/329502/85325
+        /// </summary>
+        /// <param name="directory"></param>
+        public static void DeleteDirectory(string directory)
+        {
+            string[] files = Directory.GetFiles(directory);
+            string[] dirs = Directory.GetDirectories(directory);
+
+            foreach (string file in files)
+            {
+                File.SetAttributes(file, FileAttributes.Normal);
+                File.Delete(file);
+            }
+
+            foreach (string dir in dirs)
+            {
+                DeleteDirectory(dir);
+            }
+
+            Directory.Delete(directory, false);
         }
     }
 }
