4b6faa7ea653b8bd102f255ead69c8828e9e5d40
pauldendulk <pauldendulk@gmail.com>
2018-01-02 13:59:19 +0100

    Updating nuget

--- Scripts/buildpackpush.bat
+++ Scripts/buildpackpush.bat
@@ -4,9 +4,9 @@ SET VERSION=%1
 SET NUGET=.\..\tools\nuget\nuget.exe
 
 CALL buildpack %VERSION%
-%NUGET% push .\..\Release\brutile.%VERSION%.nupkg 
-%NUGET% push .\..\Release\brutile.desktop.%VERSION%.nupkg 
-%NUGET% push .\..\Release\brutile.mbtiles.%VERSION%.nupkg 
+%NUGET% push .\..\Release\brutile.%VERSION%.nupkg -source nuget.org
+%NUGET% push .\..\Release\brutile.desktop.%VERSION%.nupkg -source nuget.org
+%NUGET% push .\..\Release\brutile.mbtiles.%VERSION%.nupkg -source nuget.org
 git commit -m %VERSION% -a
 git tag %VERSION%
 git push
