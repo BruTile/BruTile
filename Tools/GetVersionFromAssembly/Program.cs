c8ecfa5c8fc7554318caebe79117165cd3b48124
pauldendulk <pauldendulk@gmail.com>
2018-01-03 19:23:18 +0100

    Adding GetVersionFromAssembly tool

---
+++ Tools/GetVersionFromAssembly/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace GetVersionFromAssembly
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                try
+                {
+                    string path = Path.GetFullPath(arg);
+                    var assembly = Assembly.LoadFile(path);
+                    FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+                    Console.Out.WriteLine(fileVersionInfo.ProductVersion);
+                }
+                catch (Exception exception)
+                {
+                    Console.Out.WriteLine(string.Format("{0}: {1}", arg, exception.Message));
+                }
+            }
+        }
+    }
+}
