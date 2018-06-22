3eb9cd124b943a42c314b88bd2e24dc74a6ed03c
Paul den Dulk <pauldendulk@gmail.com>
2018-06-22 17:00:18 +0200

    Adding comments to build script

--- Build/deploy.cmd
+++ Build/deploy.cmd
@@ -1,4 +1,6 @@
-// REM call this script without arguments from the sln folder.
+REM edit BruTile.Common.props to set the wanted version
+REM Put the sln in Release mode and rebuild all. The version is retrieved from the Release version of BruTile.dll
+REM call this script without arguments from the sln folder.
 call dotnet build Tools\GetVersionFromAssembly\GetVersionFromAssembly.csproj /property:Configuration=Release
 for /f "delims=" %%i in ('dotnet Tools\GetVersionFromAssembly\bin\Release\netcoreapp2.0\GetVersionFromAssembly.dll .\Release\netstandard1.1\BruTile.dll') do set VERSION=%%i
 ECHO %VERSION%
