740c938c3d04389d53b801a9f6ee373add41c511
pauldendulk <pauldendulk@gmail.com>
2020-06-06 10:36:16 +0200

    And add the remainer of deploy.cmd

--- Build/deploy.cmd
+++ Build/deploy.cmd
@@ -5,3 +5,15 @@ call dotnet build Tools\GetVersionFromAssembly\GetVersionFromAssembly.csproj /pr
 REM todo: Call run on the csproj. That is possible nowadays.
 for /f "delims=" %%i in ('dotnet Tools\GetVersionFromAssembly\bin\Release\netcoreapp2.1\GetVersionFromAssembly.dll .\Release\netstandard1.1\BruTile.dll') do set VERSION=%%i
 ECHO VERSION is: %VERSION%
+
+msbuild brutile.sln /property:Configuration=Release
+
+nuget push .\Release\brutile.%VERSION%.nupkg -source nuget.org
+nuget push .\Release\brutile.mbtiles.%VERSION%.nupkg -source nuget.org
+nuget push .\Release\brutile.desktop.%VERSION%.nupkg -source nuget.org
+nuget push .\Release\brutile.desktop.DbCache.%VERSION%.nupkg -source nuget.org
+git commit -m %VERSION% -a
+git tag %VERSION%
+git push origin %VERSION%
+git push
+
