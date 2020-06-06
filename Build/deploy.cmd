9792e52235686245dc029ea3e6039a0e4d313eb7
pauldendulk <pauldendulk@gmail.com>
2020-06-06 10:35:29 +0200

    Fix bug in deploy.cmd

--- Build/deploy.cmd
+++ Build/deploy.cmd
@@ -2,14 +2,6 @@ REM edit BruTile.Common.props to set the wanted version
 REM Put the sln in Release mode and rebuild all. The version is retrieved from the Release version of BruTile.dll
 REM call this script without arguments from the sln folder.
 call dotnet build Tools\GetVersionFromAssembly\GetVersionFromAssembly.csproj /property:Configuration=Release
-for /f "delims=" %%i in ('dotnet Tools\GetVersionFromAssembly\bin\Release\netcoreapp2.0\GetVersionFromAssembly.dll .\Release\netstandard1.1\BruTile.dll') do set VERSION=%%i
-ECHO %VERSION%
-msbuild brutile.sln /property:Configuration=Release
-nuget push .\Release\brutile.%VERSION%.nupkg -source nuget.org
-nuget push .\Release\brutile.mbtiles.%VERSION%.nupkg -source nuget.org
-nuget push .\Release\brutile.desktop.%VERSION%.nupkg -source nuget.org
-nuget push .\Release\brutile.desktop.DbCache.%VERSION%.nupkg -source nuget.org
-git commit -m %VERSION% -a
-git tag %VERSION%
-git push origin %VERSION%
-git push
+REM todo: Call run on the csproj. That is possible nowadays.
+for /f "delims=" %%i in ('dotnet Tools\GetVersionFromAssembly\bin\Release\netcoreapp2.1\GetVersionFromAssembly.dll .\Release\netstandard1.1\BruTile.dll') do set VERSION=%%i
+ECHO VERSION is: %VERSION%
