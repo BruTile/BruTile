95be015727facc27d5f3e74633e2aab2dc19a5dd
pauldendulk <pauldendulk@gmail.com>
2020-06-07 13:29:22 +0200

    Fix wms called tiled sample

--- Samples/BruTile.Samples.Common/Samples/TileSourceForWmsSample.cs
+++ Samples/BruTile.Samples.Common/Samples/TileSourceForWmsSample.cs
@@ -10,10 +10,10 @@ namespace BruTile.Samples.Common.Samples
     {
         public static ITileSource Create()
         {
-            const string url = "http://geodata.nationaalgeoregister.nl/ahn25m/wms?service=wms";
+            const string url = "http://geodata.nationaalgeoregister.nl/omgevingswarmte/wms?SERVICE=WMS&VERSION=1.1.1";
             // You need to know the schema. This can be a problem. Usally it is GlobalSphericalMercator
             var schema = new WkstNederlandSchema { Format = "image/png" };
-            var request = new WmscRequest(new Uri(url), schema, new[] { "ahn25m" }.ToList(), new string[0].ToList());
+            var request = new WmscRequest(new Uri(url), schema, new[] { "koudegeslotenwkobuurt" }.ToList(), new string[0].ToList());
             var provider = new HttpTileProvider(request);
             return new TileSource(provider, schema);
         }
