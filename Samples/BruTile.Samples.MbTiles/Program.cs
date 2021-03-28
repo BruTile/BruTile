accb0d9282d8d6073d8789e85e1165b770ef9fa8
pauldendulk <pauldendulk@gmail.com>
2021-03-28 16:26:15 +0200

    BruTile.Samples.MbTiles to new csproj and netcoreapp3.1

--- Samples/BruTile.Samples.MbTiles/Program.cs
+++ Samples/BruTile.Samples.MbTiles/Program.cs
@@ -1,18 +1,21 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+using BruTile.Samples.MbTiles;
 
-namespace BruTile.Samples.MbTiles
+namespace BruTile.Samples.MbTiles2
 {
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            SQLitePCL.Batteries.Init();
-            
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MbTilesForm());
