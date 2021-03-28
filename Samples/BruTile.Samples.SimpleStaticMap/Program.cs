1c822e95d62e20ffeb816e01f11e27eb2eb00162
pauldendulk <pauldendulk@gmail.com>
2021-03-28 16:51:46 +0200

    SimpleStaticMap to the new csproj format and 3.1

--- Samples/BruTile.Samples.SimpleStaticMap/Program.cs
+++ Samples/BruTile.Samples.SimpleStaticMap/Program.cs
@@ -1,16 +1,18 @@
-﻿using System;
+using System;
 using System.Windows.Forms;
+using BruTile.Samples.SimpleStaticMap;
 
-namespace BruTile.Samples.SimpleStaticMap
+namespace BruTile.Samples.SimpleStaticMap1
 {
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
