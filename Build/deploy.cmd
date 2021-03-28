b4041063c631614bfbe22104835feab94a0e9846
pauldendulk <pauldendulk@gmail.com>
2021-03-28 15:11:32 +0200

    BruTile and BruTile.MbTiles to netstandard 2.0 and delete Desktop and Desktop.Cache projects

--- Build/deploy.cmd
+++ Build/deploy.cmd
@@ -3,15 +3,13 @@ REM Put the sln in Release mode and rebuild all. The version is retrieved from t
 REM call this script without arguments from the sln folder.
 call dotnet build Tools\GetVersionFromAssembly\GetVersionFromAssembly.csproj /property:Configuration=Release
 REM todo: Call run on the csproj. That is possible nowadays.
-for /f "delims=" %%i in ('dotnet Tools\GetVersionFromAssembly\bin\Release\netcoreapp2.1\GetVersionFromAssembly.dll .\Release\netstandard1.1\BruTile.dll') do set VERSION=%%i
+for /f "delims=" %%i in ('dotnet Tools\GetVersionFromAssembly\bin\Release\netcoreapp3.1\GetVersionFromAssembly.dll .\Release\netstandard2.0\BruTile.dll') do set VERSION=%%i
 ECHO VERSION is: %VERSION%
 
 msbuild brutile.sln /property:Configuration=Release
 
 nuget push .\Release\brutile.%VERSION%.nupkg -source nuget.org
 nuget push .\Release\brutile.mbtiles.%VERSION%.nupkg -source nuget.org
-nuget push .\Release\brutile.desktop.%VERSION%.nupkg -source nuget.org
-nuget push .\Release\brutile.desktop.DbCache.%VERSION%.nupkg -source nuget.org
 git commit -m %VERSION% -a
 git tag %VERSION%
 git push origin %VERSION%
