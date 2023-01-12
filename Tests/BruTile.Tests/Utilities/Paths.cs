2ce24a56b1640c1f649800195579dc56820063ab
pauldendulk <pauldendulk@gmail.com>
2023-01-12 20:03:17 +0100

    Remove unused usings

--- Tests/BruTile.Tests/Utilities/Paths.cs
+++ Tests/BruTile.Tests/Utilities/Paths.cs
@@ -1,6 +1,5 @@
 ﻿// Copyright (c) BruTile developers team. All rights reserved. See License.txt in the project root for license information.
 
-using System;
 using System.IO;
 using System.Reflection;
 
