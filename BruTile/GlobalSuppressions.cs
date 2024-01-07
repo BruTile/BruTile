30f8005336b2f879940ee0bdb2ec818b3e903104
Inforithmics <thomas.stocker@gmail.com>
2024-01-07 17:43:13 +0100

    Use Microsoft.XmlSerializer.Generator for Generating XMLSerializers. During Build

---
+++ BruTile/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Trimming", "IL2026:Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application code", Justification = "<Pending>", Scope = "member", Target = "~M:BruTile.Tms.TileMapParser.CreateTileSource(System.IO.Stream,System.String,System.Collections.Generic.Dictionary{System.String,System.String},BruTile.Cache.IPersistentCache{System.Byte[]},System.Func{System.Uri,System.Threading.Tasks.Task{System.Byte[]}})~BruTile.TileSource")]
+[assembly: SuppressMessage("Trimming", "IL2026:Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application code", Justification = "<Pending>", Scope = "member", Target = "~M:BruTile.Wmts.WmtsParser.Parse(System.IO.Stream,BruTile.Wmts.BoundingBoxAxisOrderInterpretation)~System.Collections.Generic.IEnumerable{BruTile.Web.HttpTileSource}")]
