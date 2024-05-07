07fbad2da9145d77b73af048d5b016706f735978
pauldendulk <pauldendulk@gmail.com>
2024-05-07 10:36:08 +0200

    Rename WmscRequest to WmscUrlBuilder

--- Tests/BruTile.Tests/Wmsc/WmscRequestTests.cs
+++ Tests/BruTile.Tests/Wmsc/WmscRequestTests.cs
@@ -13,9 +13,9 @@ internal class WmscRequestTests
     [Test]
     public void WmscRequest_NoVersion()
     {
-        var request = new WmscRequest(new Uri("http://testserver.com"), new GlobalSphericalMercator(YAxis.TMS), ["Layer One"]);
+        var wmscUrlBuilder = new WmscUrlBuilder(new Uri("http://testserver.com"), new GlobalSphericalMercator(YAxis.TMS), ["Layer One"]);
         var ti = new TileInfo { Index = new TileIndex(0, 0, 0) };
-        var uri = request.GetUrl(ti);
+        var uri = wmscUrlBuilder.GetUrl(ti);
         StringAssert.DoesNotContain("VERSION=", uri.ToString());
         StringAssert.Contains("SRS=", uri.ToString());
     }
@@ -23,7 +23,7 @@ internal class WmscRequestTests
     [Test]
     public void WmscRequest_Version111()
     {
-        var request = new WmscRequest(new Uri("http://testserver.com"), new GlobalSphericalMercator(YAxis.TMS), ["Layer One"], null, null, "1.1.1");
+        var request = new WmscUrlBuilder(new Uri("http://testserver.com"), new GlobalSphericalMercator(YAxis.TMS), ["Layer One"], null, null, "1.1.1");
         var ti = new TileInfo { Index = new TileIndex(0, 0, 0) };
         var uri = request.GetUrl(ti);
         StringAssert.Contains("VERSION=1.1.1", uri.ToString());
@@ -33,7 +33,7 @@ internal class WmscRequestTests
     [Test]
     public void WmscRequest_Version130()
     {
-        var request = new WmscRequest(new Uri("http://testserver.com"), new GlobalSphericalMercator(YAxis.TMS), ["Layer One"], null, null, "1.3.0");
+        var request = new WmscUrlBuilder(new Uri("http://testserver.com"), new GlobalSphericalMercator(YAxis.TMS), ["Layer One"], null, null, "1.3.0");
         var ti = new TileInfo { Index = new TileIndex(0, 0, 0) };
         var uri = request.GetUrl(ti);
         StringAssert.Contains("VERSION=1.3.0", uri.ToString());
