82afc6bba6e2ff683ae46f3bd1726281c526e7a7
pauldendulk <pauldendulk@gmail.com>
2024-07-05 19:56:44 +0200

    Fix ordering of usings

--- BruTile/ITileProvider.cs
+++ BruTile/ITileProvider.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) BruTile developers team. All rights reserved. See License.txt in the project root for license information.
 
-using System.Threading.Tasks;
 using System;
+using System.Threading.Tasks;
 
 namespace BruTile;
 
